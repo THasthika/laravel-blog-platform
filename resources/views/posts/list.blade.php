a907a00d3d675a1dfcb39bb9f403442f06fc2fef
Tharindu <tharindubathigama@gmail.com>
2023-04-13 19:04:59 +0100

    update post button

--- resources/views/posts/list.blade.php
+++ resources/views/posts/list.blade.php
@@ -1,9 +1,9 @@
 <x-app-layout>
 <div class="mx-auto container px-2 mt-4">
-    <div class="flex items-center">
+    <div class="flex items-start">
         <div class="flex-1 text-2xl">{{__("All Posts")}}</div>
         <div class="pr-2 pt-2">
-            <a href="{{route('post.create')}}" class="btn">Post +</a>
+            <a href="{{route('post.create')}}" class="btn btn-primary">Post +</a>
         </div>
     </div>
     <div class="">
