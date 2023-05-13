2df49e3de04b8674ac334c8469f6c461194722a4
Tharindu <tharindubathigama@gmail.com>
2023-05-13 20:11:44 +0100

    fix: hide add posts button when logged out

--- resources/views/posts/list.blade.php
+++ resources/views/posts/list.blade.php
@@ -2,9 +2,11 @@
 <div class="mx-auto container px-2 mt-4">
     <div class="flex items-start">
         <div class="flex-1 text-2xl">{{__("All Posts")}}</div>
+        @if (Auth::user())
         <div class="pr-2 pt-2">
             <a href="{{route('post.create')}}" class="btn btn-primary">Post +</a>
-        </div>
+        </div>   
+        @endif
     </div>
     <div class="">
         @foreach($posts as $post)
